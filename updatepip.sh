afc4876a60b3b54df37bcda3862b16b9a12e1e97
Luke Shingles <luke.shingles@gmail.com>
2017-03-19 10:25:28 +0000

    Enable compatibility with future versions of pip

--- updatepip.sh
+++ updatepip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-pip2 list --outdated | cut -d ' ' -f1 | xargs -n1 pip2 install -U
-pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U
+pip2 list --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 pip2 install -U
+pip3 list --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 pip3 install -U
