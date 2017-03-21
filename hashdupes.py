6a729096d3480763a5a251fa6bdd0fd6a009cb4f
Luke Shingles <luke.shingles@gmail.com>
2017-03-21 15:39:23 +0000

    Add arguments to hash dupes

--- hashdupes.py
+++ hashdupes.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
+import argparse
 import glob
 import hashlib
 import os
+import time
 from collections import defaultdict
 from functools import partial
 
 
-"""
-    Print out all files in the current directory that have duplicate hashes
-"""
-
-
-def hashfile(filename):
+def getfilehash(filename):
     with open(filename, mode='rb') as f:
         d = hashlib.sha1()
         for buf in iter(partial(f.read, 128 * 1024), b''):
@@ -19,26 +16,34 @@ def hashfile(filename):
     return d.hexdigest()
 
 
-filelist = glob.glob("*", recursive=False)
+def main():
+    parser = argparse.ArgumentParser(description='Find files with duplicate hashes')
+    parser.add_argument('files', nargs='*', default=glob.glob("*"),
+                        help='list of files to check (default: *)')
+    args = parser.parse_args()
 
-hashdict = defaultdict(list)
-for filepath in filelist:
-    if not os.path.isfile(filepath):
-        continue
-    strhash = hashfile(filepath)
-    hashdict[strhash].append(filepath)
-    # print(strhash, filepath)
+    hashdict = defaultdict(list)
+    for filepath in args.files:
+        if not os.path.isfile(filepath):
+            continue
+        strhash = getfilehash(filepath)
+        hashdict[strhash].append(filepath)
+        # print(strhash, filepath)
 
-firstdupehash = True
-for strhash, filematches in hashdict.items():
-    if len(filematches) > 1:
-        if not firstdupehash:
-            print()
-        firstdupehash = False
+    firstdupehash = True
+    for strhash, filematches in hashdict.items():
+        if len(filematches) > 1:
+            if not firstdupehash:
+                print()
+            firstdupehash = False
 
-        for index, filematch in enumerate(filematches):
-            columnone = strhash if index == 0 else "".ljust(len(strhash))
-            print(f"{columnone}  {filematch}")
+            for index, filematch in enumerate(filematches):
+                columnone = strhash if index == 0 else "".ljust(len(strhash))
+                print(f"{columnone}  {time.ctime(os.path.getmtime(filematch))}  {filematch}")
 
-if firstdupehash:
-    print("No duplicates!")
+    if firstdupehash:
+        print("No duplicates!")
+
+
+if __name__ == "__main__":
+    main()
