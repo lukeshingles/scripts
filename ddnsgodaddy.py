382d13cf6eb099b9766192b588b6d7e4e17204f0
Luke Shingles <luke.shingles@gmail.com>
2019-08-16 17:34:01 +0100

    Update ddnsgodaddy.py

--- ddnsgodaddy.py
+++ ddnsgodaddy.py
@@ -12,11 +12,17 @@ def validipaddress(ipaddress):
         return False # not a string
 
 
-def printdnsrecords(apiurl, headers, prefix=""):
+def printdnsrecords(domain, hostname, headers, prefix="", ipmatch=None):
+    apiurl = f"https://api.godaddy.com/v1/domains/{domain}/records/A/{hostname}"
+    updaterequired = True
     with requests.get(apiurl, headers=headers) as r:
         assert(r.status_code == 200)
         for record in r.json():
             print(f"{prefix:25s}{record['name']}  {record['type']}  {record['data']}  TTL: {record['ttl']}")
+            if record['data'] == ipmatch:
+                updaterequired = False
+
+    return updaterequired
 
 
 def main():
@@ -46,15 +52,16 @@ def main():
 
     print(f"External IP address is {ipaddress}")
 
-    apiurl = f"https://api.godaddy.com/v1/domains/{domain}/records/A/{hostname}"
+    updaterequired = printdnsrecords(domain, hostname, authheader, prefix="GoDaddy DNS before: ", ipmatch=ipaddress)
 
-    printdnsrecords(apiurl, authheader, prefix="GoDaddy DNS before: ")
+    if not updaterequired:
+            print("(no update needed)")
+    else:
+        newrecords = [{ "data": ipaddress, "ttl": ttl, "name": hostname, "type": "A" },]
+        with requests.put(apiurl, headers=authheader, json=newrecords) as r:
+            assert(r.status_code == 200)
 
-    newrecords = [{ "data": ipaddress, "ttl": ttl, "name": hostname, "type": "A" },]
-    with requests.put(apiurl, headers=authheader, json=newrecords) as r:
-        assert(r.status_code == 200)
-
-    printdnsrecords(apiurl, authheader, prefix="GoDaddy DNS after: ")
+        printdnsrecords(domain, hostname, authheader, prefix="GoDaddy DNS after: ")
 
     print("\n-----\n")
 
