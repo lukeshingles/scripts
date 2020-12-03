b5557a3b1911cbbd6251227d81c9154c7445a948
Luke Shingles <luke.shingles@gmail.com>
2020-12-03 16:59:55 +0000

    Remove --use-feature=2020-resolver

--- updatepip.sh
+++ updatepip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 set -x #echo on
 
-python3 -m pip list --use-feature=2020-resolver --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 python3 -m pip install --use-feature=2020-resolver -U
+python3 -m pip list --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 python3 -m pip install -U
