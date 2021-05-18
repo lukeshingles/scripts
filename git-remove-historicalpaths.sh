a67fa63da744ebd83aed5300ee2d8324480498e1
Luke Shingles <luke.shingles@gmail.com>
2021-05-18 11:14:12 +0100

    Create git-remove-historicalpaths.sh

---
+++ git-remove-historicalpaths.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+# commented because this operation can be destructive
+echo "uncomment this script to use"
+# git filter-branch --force --index-filter \
+#   "git rm --cached --ignore-unmatch MY-PATH-TO-DELETE" \
+#   --prune-empty --tag-name-filter cat -- --all
