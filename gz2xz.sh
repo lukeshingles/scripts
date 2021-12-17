ebb537bc216aa2d7e9da3694c8df450632db1d46
Luke Shingles <luke.shingles@gmail.com>
2021-12-17 14:11:08 +0000

    Update gz2xz.sh

---
+++ gz2xz.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env zsh
+set -e
+set -o pipefail
+
+#!/usr/bin/env zsh
+
+if [ $# -eq 0 ]; then
+  echo 1>&2 "Usage: $0 [file ...]"
+  exit 3
+fi
+
+for file in "$@"
+do
+  if [[ -f "$file" ]]; then
+    if [[ ${file##*\.}  == 'xz' ]]; then
+      echo $file is already xzipped
+      if xz -t "$file"; then
+        echo $file is good!
+      else
+        echo $file is bad!
+      fi
+    else
+      if [[ ${file##*\.}  == 'gz' ]]; then
+        fileorig=${file%.gz}
+        echo decompressing $file
+        gunzip "$file"
+      else
+        fileorig=$file
+      fi
+
+      filexz=$fileorig.xz
+
+      ORIGSUM=$(sha1sum < $fileorig)
+
+      xz --best -T8 -v $fileorig
+
+      NEWSUM=$(unxz -c $filexz | sha1sum)
+
+      # if xz -t "$filexz"; then
+      #   echo $filexz is good according to xz
+      #   if [ "${ORIGSUM}" = "${NEWSUM}" ]; then
+      #     rm $fileorig
+      #     echo checksum matched. deleted $fileorig
+      #   else
+      #     echo checksum mismatch. not deleting $fileorig
+      #   fi
+      # fi
+    fi
+  else
+    echo File "$file" does not exist
+  fi
+done
+
