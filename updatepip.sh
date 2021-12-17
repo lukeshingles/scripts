e50565223433f2564ad18e4c65271cf76e24651f
Luke Shingles <luke.shingles@gmail.com>
2021-12-17 13:41:06 +0000

    Update updatepip.sh

--- updatepip.sh
+++ updatepip.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env zsh
-set -x #echo on
+# set -x #echo on
 
-declare -a arr=("python3.8" "python3.9" "python3.10" "pypy3")
+declare -a arr=("pypy3" "python3.10")
 
-## now loop through the above array
+# now loop through the above array
 for pyver in "${arr[@]}"
 do
    $pyver -m pip list --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 $pyver -m pip install --upgrade
 done
-
-
-#python3 -m pip list --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 python3 -m pip install --upgrade
