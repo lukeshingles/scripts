3af1f8499a00911fe3e72a5a3173e5affc468e49
Luke Shingles <luke.shingles@gmail.com>
2021-12-20 08:57:08 +0000

    Update gz2xz.sh

--- gz2xz.sh
+++ gz2xz.sh
@@ -14,27 +14,38 @@ do
   if [[ -f "$file" ]]; then
     if [[ ${file##*\.}  == 'xz' ]]; then
       echo $file is already xzipped
-      if xz -t "$file"; then
-        echo $file is good!
-      else
-        echo $file is bad!
-      fi
+      # if xz -t "$file"; then
+      #   echo $file is good!
+      # else
+      #   echo $file is bad!
+      # fi
     else
       if [[ ${file##*\.}  == 'gz' ]]; then
         fileorig=${file%.gz}
         echo decompressing $file
+        du -h $file
         gunzip "$file"
       else
         fileorig=$file
       fi
 
+      du -h $fileorig
+
       filexz=$fileorig.xz
 
-      ORIGSUM=$(sha1sum < $fileorig)
+      # ORIGSUM=$(sha1sum < $fileorig)
 
-      xz --best -T8 -v $fileorig
+      if [[ -f "$filexz" ]]; then
+        read "confirmoverwrite?$filexz already exists! Overwrite? [y]"
+        if [[ "$confirmoverwrite" =~ ^[Yy]$ ]]
+        then
+          overwriteflag="-f"
+        fi
+      fi
+      xz --best -T0 $overwriteflag -v $fileorig
+      du -h $filexz
 
-      NEWSUM=$(unxz -c $filexz | sha1sum)
+      # NEWSUM=$(unxz -c $filexz | sha1sum)
 
       # if xz -t "$filexz"; then
       #   echo $filexz is good according to xz
