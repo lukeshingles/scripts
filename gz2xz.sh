85d33b866caacebd5d4908d96febddf9491f77c6
Luke Shingles <luke.shingles@gmail.com>
2022-01-03 15:03:10 +0000

    Update gz2xz.sh

--- gz2xz.sh
+++ gz2xz.sh
@@ -9,6 +9,21 @@ if [ $# -eq 0 ]; then
   exit 3
 fi
 
+
+function trap_ctrlc() {
+   # perform cleanup here
+
+   if [[ -f "$incompletefile" ]]; then
+     rm $incompletefile
+     echo "Ctrl-C caught...deleted incomplete file: $incompletefile"
+   fi
+
+   # exit shell script with error code 2
+   exit 2
+}
+
+trap "trap_ctrlc" 2
+
 for file in "$@"
 do
   if [[ -f "$file" ]]; then
@@ -20,45 +35,59 @@ do
       #   echo $file is bad!
       # fi
     else
+      echo " "
+      du -h $file
       if [[ ${file##*\.}  == 'gz' ]]; then
-        fileorig=${file%.gz}
-        echo decompressing $file
-        du -h $file
-        gunzip "$file"
+        filexz=${file%.gz}.xz
       else
-        fileorig=$file
+        filexz=$file.xz
       fi
 
-      du -h $fileorig
-
-      filexz=$fileorig.xz
-
-      # ORIGSUM=$(sha1sum < $fileorig)
-
+      SKIPFILE=false
       if [[ -f "$filexz" ]]; then
         read "confirmoverwrite?$filexz already exists! Overwrite? [y]"
         if [[ "$confirmoverwrite" =~ ^[Yy]$ ]]
         then
-          overwriteflag="-f"
+          SKIPFILE=false
+        else
+          SKIPFILE=true
         fi
       fi
-      xz --best -T0 $overwriteflag -v $fileorig
-      du -h $filexz
 
-      # NEWSUM=$(unxz -c $filexz | sha1sum)
+      if [ "$SKIPFILE" = false ]; then
+        if [[ ${file##*\.}  == 'gz' ]]; then
+          # uncompress gzip and compress xzip
+          ORIGSUM=$(gunzip -c $file | sha1sum)
+          echo "$ORIGSUM (gzip uncompressed file checksum)"
 
-      # if xz -t "$filexz"; then
-      #   echo $filexz is good according to xz
-      #   if [ "${ORIGSUM}" = "${NEWSUM}" ]; then
-      #     rm $fileorig
-      #     echo checksum matched. deleted $fileorig
-      #   else
-      #     echo checksum mismatch. not deleting $fileorig
-      #   fi
-      # fi
+          incompletefile=$filexz
+          gunzip < "$file" | xz -T0 -f -v --best > "$filexz"
+          incompletefile=""
+
+          NEWSUM=$(unxz -c $filexz | sha1sum)
+          echo "$NEWSUM (new xzip uncompressed checksum)"
+
+          if xz -t "$filexz"; then
+            if [ "${ORIGSUM}" = "${NEWSUM}" ]; then
+              rm $file
+              echo "$filexz is good and checksum matched. Deleted $file"
+            else
+              rm $filexz
+              echo "ERROR: checksum mismatch! Did not delete $file"
+              read "Press enter to continue"
+            fi
+          else
+            rm $filexz
+            echo "ERROR: $filexz is bad according to xz!"
+            read "Press enter to continue"
+          fi
+        else
+          xz -T0 -f -v --best $file
+        fi
+      fi
     fi
   else
-    echo File "$file" does not exist
+    echo "File $file does not exist"
   fi
 done
 
