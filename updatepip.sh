37cf34124bcb512357bba9278eeac62ae0821ddd
Luke Shingles <luke.shingles@gmail.com>
2022-09-09 14:03:14 +0100

    Update updatepip.sh

--- updatepip.sh
+++ updatepip.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
 # set -x #echo on
 
-declare -a arr=("pypy3" "python3.9" "python3.10")
-# declare -a arr=("python3.10")
+#declare -a arr=("python3.9" "python3.10")
+
+declare -a arr=("python3.10" "python3.11")
 
 # now loop through the above array
 for pyver in "${arr[@]}"
 do
-   $pyver -m pip list --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 $pyver -m pip install --upgrade
+   $pyver -m pip list --format=freeze --outdated | cut -d '=' -f1 | xargs -n1 $pyver -m pip install --use-deprecated legacy-resolver --upgrade
 done
