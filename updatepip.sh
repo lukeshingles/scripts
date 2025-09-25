b9f4fe56a98265343e7b619a7e4a1dc68e14be6e
Luke Shingles <luke.shingles@gmail.com>
2025-09-25 14:31:06 +0100

    Update updatepip.sh

--- updatepip.sh
+++ updatepip.sh
@@ -1,16 +1,13 @@
-#!/usr/bin/env zsh
-# set -x #echo on
+#!/bin/zsh
+set -x #echo on
 
-#declare -a arr=("python3.9" "python3.10")
+pyver=("python3")
+#pyver=("python3.12" "python3.13")
 
-# declare -a arr=("python3.10" "python3.11")
-declare -a arr=("python3.11")
+for i in {1..$#pyver}; do
 
-#--use-deprecated legacy-resolver
+    ${pyver[i]} -m uv pip list --outdated --exclude-editable --exclude artistools --exclude pynonthermal | tee /dev/tty | tail -n +3 | cut -w -f1 | xargs -n1 ${pyver[i]} -m uv pip install --upgrade
 
-# now loop through the above array
-#$pyver -m pip install --upgrade
-for pyver in "${arr[@]}"
-do
-    $pyver -m pip list freeze --outdated | tail -n +3 | cut -w -f1 | xargs -n1 $pyver -m pip install --upgrade
 done
+
+uv pip install --upgrade --pre ty
